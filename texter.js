53c1ffcb760ea0e50763b15b6a7a8e193d551afd
clovr <ramenpeppina@gmail.com>
2025-11-28 12:23:08 -0500

    all good   now?

--- texter.js
+++ texter.js
@@ -322,9 +322,9 @@ function box_stack_update()
 	for (let i = 0; i < bonus_boxes.length; i++) 
 	{
 		if(marge.checked)
-  		{ctx_stack.drawImage(bonus_boxes[i], box_size[3]*(i%stack_width), (box_size[3] + 12)*Math.floor(i/box_size[3]*(i%stack_width)))}
+  		{ctx_stack.drawImage(bonus_boxes[i], box_size[2]*(i%stack_width), (box_size[3] + 12)*Math.floor(i/stack_width))}
 		else
-  		{ctx_stack.drawImage(bonus_boxes[i], 6+(6+box_size[3])*(i%stack_width), 6+(box_size[3] + 12)*Math.floor(i/box_size[3]*(i%stack_width))}
+  		{ctx_stack.drawImage(bonus_boxes[i], 6+(6+box_size[2])*(i%stack_width), 6+(box_size[3] + 12)*Math.floor(i/stack_width))}
 	}
 
 	const dataURL = canvas_stack.toDataURL('image/png');
