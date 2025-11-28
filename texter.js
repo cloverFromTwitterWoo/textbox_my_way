6340bfaf88c03f9ca24484f33152d05cde71746d
clovr <ramenpeppina@gmail.com>
2025-11-28 12:08:37 -0500

    test?

--- texter.js
+++ texter.js
@@ -62,6 +62,8 @@ let font_dt_mono_dw = loadImage("assets/determination_mono_dw.png")
 
 let boxes_in = []
 
+let stack_width_inp = document.getElementById(id="stack_width")
+
 ctx.imageSmoothingEnabled = false
 canvas.imageSmoothingEnabled = false
 
@@ -313,15 +315,16 @@ function draw_canvas()
 function box_stack_update()
 {
 	//alert(box_size)
+	var stack_width = stack_width_inp
 	canvas_stack.height = (box_size[3] + 12)*bonus_boxes.length
-	canvas_stack.width = (box_size[2] + 12)
+	canvas_stack.width = (box_size[2] + 12)*stack_width_inp
 	ctx_stack.fillRect(0,0,canvas_stack.width,canvas_stack.height)
 	for (let i = 0; i < bonus_boxes.length; i++) 
 	{
 		if(marge.checked)
-  		{ctx_stack.drawImage(bonus_boxes[i], 0, (box_size[3] + 12)*i)}
+  		{ctx_stack.drawImage(bonus_boxes[i], box_size[3]*(i%stack_width_inp), (box_size[3] + 12)*Math.floor(i/box_size[3]*(i%stack_width_inp)))}
 		else
-  		{ctx_stack.drawImage(bonus_boxes[i], 6, 6+(box_size[3] + 12)*i)}
+  		{ctx_stack.drawImage(bonus_boxes[i], 6+(6+box_size[3])*(i%stack_width_inp), 6+(box_size[3] + 12)*Math.floor(i/box_size[3]*(i%stack_width_inp))}
 	}
 
 	const dataURL = canvas_stack.toDataURL('image/png');
